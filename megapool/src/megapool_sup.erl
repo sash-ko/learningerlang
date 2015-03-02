d269642ac4bd70a2f8a7eb1603790f91327cc144
sash-ko <sashkolysenko@gmail.com>
2015-03-02 15:02:08 +0100

    supervisor stop function

--- megapool/src/megapool_sup.erl
+++ megapool/src/megapool_sup.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, stop/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -16,7 +16,16 @@
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	%% The supervisor registered locally as "megapool"
+    supervisor:start_link({local, megapool}, ?MODULE, []).
+
+stop() ->
+	case whereis(megapool) of
+		P when is_pid(P) ->
+			exit(P, kill);
+		_ -> ok
+	end. 
+
 
 %% ===================================================================
 %% Supervisor callbacks
