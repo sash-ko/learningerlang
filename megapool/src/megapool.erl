dd195364ad0d576c60050659ed4832e59e000aa8
sash-ko <sashkolysenko@gmail.com>
2015-03-03 16:07:50 +0100

    removed redundant functions

--- megapool/src/megapool.erl
+++ megapool/src/megapool.erl
@@ -1,12 +1,5 @@
 -module(megapool).
--export([start/2, stop/1, start_pool/3,
-			run/2, sync_queue/2, async_queue/2, stop_pool/1]).
- 
-start(normal, _Args) ->
-	megapool_sup:start_link().
- 
-stop(_State) ->
-	ok.
+-export([start_pool/3, run/2, sync_queue/2, async_queue/2, stop_pool/1]).
  
 start_pool(Name, Limit, {M,F,A}) ->
 	megapool_sup:start_pool(Name, Limit, {M,F,A}).
